93d5dd33c6685a7202d677f3342067bf4c7b8e13
stiven-paspur2 <stivenpaspur@gmail.com>
2021-10-27 16:29:16 -0500

    Comunicacion padre a hija

---
+++ src/app/selectornumerico/selectornumerico.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selectornumerico',
+  templateUrl: './selectornumerico.component.html',
+  styleUrls: ['./selectornumerico.component.css']
+})
+export class SelectornumericoComponent implements OnInit {
+  @Input() minimo!:number|String;
+  @Input() maximo!:number|String;
+  actual!: number;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.actual=Number(this.minimo);
+  }
+  incrementar(){
+    if(this.actual<this.maximo){
+      this.actual ++;
+    }
+  }
+  decrementar(){
+    if(this.actual>this.minimo)
+    this.actual--;
+  }
+  fijar(v:number){
+    if(v>=this.minimo && v<=this.maximo){
+      this.actual=v;
+    }
+  }
+}
